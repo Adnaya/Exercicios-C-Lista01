2662683f316dc8726052fa57df5262c3f1465336
Amanda <amanda.fralve@gmail.com>
2024-06-07 12:57:23 -0300

    ex-03

---
+++ exercicio-03/main.c
@@ -0,0 +1,26 @@
+Atualizações nos atalhos do teclado … Em quinta-feira, 1 de agosto de 2024, os atalhos de teclado do Drive serão atualizados para oferecer a navegação por letras iniciais.Saiba mais
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Exercício 3*/
+/* Crie um algoritmo que leia o valor do salário-mínimo e o valor do salário de
+um usuário, calcule a quantidade de salários-mínimos que esse usuário ganha e
+imprima o resultado. Considere que um salário-mínimo equivale a R$ 1320,00. */
+
+int main(){
+
+    setlocale(LC_ALL,"Portuguese");
+    float salario, salarioMin;
+
+    printf("Insira o valor do seu salario:\n");
+    scanf("%f",&salario);
+
+    salarioMin = (salario/1320);
+
+    printf("Você recebe %f salário(s) minimo(s)",salarioMin);
+
+    return 0;
+
+}
