e1c679dc47655414ed42f6e1ef473666841a0be9
Amanda <amanda.fralve@gmail.com>
2024-06-07 12:40:46 -0300

    ex-1

---
+++ exercicio-01/main.c
@@ -0,0 +1,22 @@
+Atualizações nos atalhos do teclado … Em quinta-feira, 1 de agosto de 2024, os atalhos de teclado do Drive serão atualizados para oferecer a navegação por letras iniciais.Saiba mais
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Exercício 01 */
+/* Desenvolva um algoritmo, utilizando pseudocódigo, que leia um número inteiro e imprima o seu antecessor e seu sucessor.*/
+
+int main(){
+
+    setlocale(LC_ALL,"Portuguese");
+    int numAnte,numSuce,numero;
+
+    printf("Para descobrir o sucessor e antecessor de um número, digite-o a seguir:\n");
+    scanf("%d",&numero);
+
+    numAnte =(numero-1);
+    numSuce =(numero+1);
+
+    printf("O numero antecessor de %d e %d, e seu sucessor e %d.",numero,numAnte,numSuce);
+}
